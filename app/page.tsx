5f5c26c50932aa65911754095059406bb55b9da9
Anjan Shomodder <thatanjan@gmail.com>
2025-01-11 23:24:58 +0600

    Setup supabase for nextjs
    Squashed commit of the following:
    
    commit cbbab662c1ea903d5a9a27437887c05df678cb22
    Author: Anjan Shomodder <thatanjan@gmail.com>
    Date:   Sat Jan 11 23:13:51 2025 +0600
    
        Setup supabase for nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import ChatLayout from './components/chat-layout'
+import ChatLayout from '@/app/components/chat-layout'
 
 export default function Home() {
   return (
-    <main className="flex h-screen bg-gray-900 text-white">
+    <main className='flex h-screen bg-gray-900 text-white'>
       <ChatLayout />
     </main>
   )
 }
-
